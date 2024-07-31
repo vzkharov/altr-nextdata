bff170f4f4a6c78af8d1d64991be1bdcc83443b1
zkharov <zakharovvadzim@yandex.by>
2024-07-31 18:43:45 +0300

    rename:

--- packages/next-wrapper/README.md
+++
@@ -1,1 +0,0 @@
-# altr-next-wrapper
--- packages/next-wrapper/src/utils/boolean.ts
+++
@@ -1,5 +0,0 @@
-const isNull = <T>(value: T | null | undefined): boolean => value === null
-
-const isUndefined = <T>(value: T | null | undefined) => typeof value === 'undefined'
-
-export { isNull, isUndefined }
